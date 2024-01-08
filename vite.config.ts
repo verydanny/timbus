ae4da94cddb36e64640a4b0dbb216cc177cbda59
Daniil Veremchuk <daniel@verydanny.com>
2024-01-08 13:55:39 +0900

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
-  // build: {
-  //   target: 'esnext',
-  //   modulePreload: {
-  //     polyfill: false,
-  //   },
-  //   cssCodeSplit: true,
-  // },
-  // esbuild: {
-  //   legalComments: 'external',
-  // },
+  build: {
+    target: 'esnext',
+    modulePreload: {
+      polyfill: false,
+    },
+    cssCodeSplit: true,
+  },
+  esbuild: {
+    legalComments: 'external',
+  },
 })
