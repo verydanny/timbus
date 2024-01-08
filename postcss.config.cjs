bc8b651fce324f5b902d946149332e318de670a1
Daniil Veremchuk <daniel@verydanny.com>
2024-01-08 13:46:56 +0900

    try cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,18 +1,24 @@
+const tailwind = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+
 /** @type { import('postcss-load-config').Config } */
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-    ...(process.env.NODE_ENV === 'production' ?
-      {
-        '@fullhuman/postcss-purgecss': {
-          content: ['./src/**/*.svelte'],
-          fontFace: true,
-          keyframes: true,
-          variables: true,
-        },
-      }
-    : {}),
-  },
+module.exports = ({ env }) => {
+  return {
+    plugins: [tailwind(), autoprefixer()],
+  }
+  // plugins: {
+  //   tailwindcss: {},
+  //   autoprefixer: {},
+  //   ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  //   ...(process.env.NODE_ENV === 'production' ?
+  //     {
+  //       '@fullhuman/postcss-purgecss': {
+  //         content: ['./src/**/*.svelte'],
+  //         fontFace: true,
+  //         keyframes: true,
+  //         variables: true,
+  //       },
+  //     }
+  //   : {}),
+  // },
 }
